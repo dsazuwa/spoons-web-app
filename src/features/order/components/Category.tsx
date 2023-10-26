c87687bba039aab3f87b6f7ab610deb0ac5f8260
dosazuwa <dsazuwa@gmail.com>
2023-10-26 18:57:19 +0100

    feat: change order/CategoryHeader to a styled component

--- src/features/order/components/Category.tsx
+++ src/features/order/components/Category.tsx
@@ -1,7 +1,29 @@
+import { styled } from '@mui/material';
 import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
-import CategoryHeader from './CategoryHeader';
+import palette from '@utils/palette';
 import ItemGrid from './ItemGrid';
+
+const CategoryHeader = styled(Typography)(({ theme }) => ({
+  color: palette.primary[900],
+  textTransform: 'uppercase',
+  fontWeight: 1000,
+  paddingLeft: '10px',
+
+  letterSpacing: 3,
+  fontSize: '1em',
+
+  [theme.breakpoints.only('xs')]: {
+    letterSpacing: 2,
+    fontSize: '0.8em',
+  },
+
+  [theme.breakpoints.only('sm')]: {
+    letterSpacing: 2.5,
+    fontSize: '0.9em',
+  },
+}));
 
 interface CategoryProps {
   index: number;
@@ -18,7 +40,7 @@ function Category({ index, menu }: CategoryProps) {
       spacing={1}
       paddingTop={{ xs: '5px', sm: '10px', md: '10px' }}
     >
-      <CategoryHeader name={category} />
+      <CategoryHeader>{category}</CategoryHeader>
       <ItemGrid items={items} />
     </Stack>
   );
